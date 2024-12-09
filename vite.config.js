bd96bd16c04907ea6d0d3739cf8faed7c25277cf
Lyn Resell <61920408+lynar13@users.noreply.github.com>
2024-12-09 00:04:58 +0000

    install gh-pages and removed leading / from rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,27 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   appType: "mpa",
-  base: "",
+  base: "/auction-website/",
   build: {
     target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "/index.html"),
-        login: resolve(__dirname, "/auth/login/index.html"),
-        register: resolve(__dirname, "/auth/register/index.html"),
-        profile: resolve(__dirname, "/profile/index.html"),
-        listing: resolve(__dirname, "/listing/index.html"),
-        editListing: resolve(__dirname, "/listing/edit/index.html"),
-        createListing: resolve(__dirname, "/listing/create/index.html"),
-        bids: resolve(__dirname, "/bids/index.html"),
-        listings: resolve(__dirname, "/listings/index.html"),
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "auth/login/index.html"),
+        register: resolve(__dirname, "auth/register/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        listing: resolve(__dirname, "listing/index.html"),
+        editListing: resolve(__dirname, "listing/edit/index.html"),
+        createListing: resolve(__dirname, "listing/create/index.html"),
+        bids: resolve(__dirname, "bids/index.html"),
+        listings: resolve(__dirname, "listings/index.html"),
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/css/variables.scss";` // Optional global SCSS variables/mixins
       },
     },
   },
